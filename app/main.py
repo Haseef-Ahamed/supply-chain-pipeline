c86cc814ca27d9ace947c52562ef58394c78bab3
Haseef-Ahamed <mshaseefat@gmail.com>
2026-05-08 12:33:28 +0530

    Demo Testing of Chain Pipeline

--- app/main.py
+++ app/main.py
@@ -10,8 +10,8 @@ app = FastAPI(
 @app.get("/")
 def root():
     return {
-        "message": "Supply Chain Security Pipeline — Testing App",
-        "version": "2.0.0",
+        "message": "Supply Chain Security Pipeline — Testing FastAPI Application",
+        "version": "3.0.0",
         "status": "running"
     }
 
